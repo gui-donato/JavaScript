cb73cd1c41fd3b3fc5e1b467e594f45e845ef860
Guilherme Donato <163470077+gui-donato@users.noreply.github.com>
2024-12-03 20:44:53 -0300

    att

---
+++ ex007/index.js
@@ -0,0 +1,14 @@
+/* var vel = 6
+console.log('Sua velocidade é de '+ vel+ ' km/h')
+if(vel > 60){
+    console.log('vc ultrapassou a velocidade miníma permitida. MULTADO!')
+}
+console.log( 'Dirija sempre usando cinto de segurança')*/
+var pais = 'Brasil'
+console.log('vivendo na ' + pais)
+if( pais == 'Brasil'){
+    console.log('parabéns, voce é brasileiro')
+} 
+else{
+    console.log('parabens, voce é estrangeiro')
+}

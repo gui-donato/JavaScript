cacdea617552d288b29f776db4e6f69634962a76
Guilherme Donato <163470077+gui-donato@users.noreply.github.com>
2024-12-04 20:05:40 -0300

    att

--- ex008/index.js
+++ ex008/index.js
@@ -1,4 +1,12 @@
-var idade = 32 
-if (idade  < 16 < 18 || > 65 ) {
-    console.log( 'V')
+var idade = 
+console.log ('Voce tem ' + idade + ' anos')
+if  ( idade < 16 ){
+    console.log(' nao vota ')
+}
+else if ( idade < 18 || idade > 65 ) {
+    console.log( 'Voto opcional')
+}
+else
+{
+    console.log ('voto obrigatório')
 }

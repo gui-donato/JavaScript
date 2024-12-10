bfd80fc1312f7bb753252cdc6ee2d75af992d4dd
Guilherme Donato <163470077+gui-donato@users.noreply.github.com>
2024-12-10 18:11:12 -0300

    att

---
+++ ex011/ambinte.js
@@ -0,0 +1,15 @@
+var c = 01
+do { console.log('eu fiz '+ c + ' vezes')
+    c++
+
+}while(c <= 10) 
+
+
+
+
+
+/*var c = 1 
+while(c <= 10){
+    console.log('eu tentei ' + c + ' vezes')
+    c++
+}0*/

285bd4fecc5a53f9f5fcbaaaf5f76ed8c34b0b92
Guilherme Donato <163470077+gui-donato@users.noreply.github.com>
2024-12-15 09:11:04 -0300

    att

---
+++ ex015/funçao.js
@@ -0,0 +1,11 @@
+/*function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'par'
+    } else { return 'impar' }
+}
+let res = parimpar(8)
+console.log(res)*/
+let v = function(x){
+    return x**2
+}
+console.log(v(9))
